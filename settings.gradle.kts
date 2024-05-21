0ed38356f677b04d04defe9ef07dc565b37d9642
Gasper Kojek <gkojek@gradle.com>
2024-05-21 15:12:59 +0200

    Bumped dv gradle plugin to 3.17.4 and gradle to 8.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,21 +10,19 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version("3.17.2")
+    id("com.gradle.develocity") version("3.17.4")
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.1"
 }
 
 val isCI = System.getenv("CI") != null
 
-gradleEnterprise {
-    allowUntrustedServer = false // ensure a trusted certificate is configured
+develocity {
+    allowUntrustedServer = true // ensure a trusted certificate is configured
 
     server = "https://ge.solutions-team.gradle.com/" // adjust to your Develocity server
 
     buildScan {
-        capture { isTaskInputFiles = true }
-        publishAlways()
-        isUploadInBackground = !isCI
+        uploadInBackground = !isCI
     }
 }
 
@@ -33,7 +31,7 @@ buildCache {
         isEnabled = true
     }
 
-    remote(gradleEnterprise.buildCache) {
+    remote(develocity.buildCache) {
         isEnabled = true
         isPush = false
     }

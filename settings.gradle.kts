d73a2f1b0be2f81942b5aba7b9ca848f01f6e3e2
Gasper Kojek <gkojek@gradle.com>
2024-05-23 11:39:54 +0200

    Java toolchain provisioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,7 @@ pluginManagement {
 plugins {
     id("com.gradle.develocity") version("3.17.4")
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
 val isCI = System.getenv("CI") != null

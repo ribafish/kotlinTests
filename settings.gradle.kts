9a267121e2a05c5bf88756a1e7d9ba229d28cc91
Gasper Kojek <gkojek@gradle.com>
2024-07-29 13:41:05 +0200

    Bump DV and CCUD plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,8 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity") version("3.17.4")
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.1"
+    id("com.gradle.develocity") version("3.17.6")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
 }
 
 val isCI = System.getenv("CI") != null

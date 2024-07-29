75533b108fb79699a558a6e03e4300a84cfa2968
Gasper Kojek <gkojek@gradle.com>
2024-07-29 13:58:10 +0200

    obfuscated key and token env vars

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,7 +10,12 @@ import kotlinx.coroutines.InternalCoroutinesApi
 fun main() {
     println("Hello, world!!!")
     println("Getting GITHUB_ACTIONS env var: ${System.getenv("GITHUB_ACTIONS")}")
-    val envVars = System.getenv().entries.map { "\t$it" }.reduce { acc, it -> "$acc\n$it" }
+    val envVars = System.getenv().entries.map { (key, value) ->
+        if (key.contains("key", ignoreCase = true) || key.contains("token", ignoreCase = true))
+            "\t$key=***"
+        else
+            "\t$key=$value"
+    }.reduce { acc, it -> "$acc\n$it" }
     println("Available env vars:\n$envVars\n")
 
     quickChecks()
